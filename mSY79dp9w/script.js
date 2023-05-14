26162194055fc10cf7ec48fcb0e59264f85f7467
Tiffany Jakowczuk <tejakowczuk@gmail.com>
2023-05-14 01:21:15 -0400

    mistakes

--- mSY79dp9w/script.js
+++ mSY79dp9w/script.js
@@ -7,11 +7,11 @@ function welcomeFade () {
     },100);
 
     setTimeout(function() {
-        document.querySelector('#topstuff p:nth-child(2)').style.transform = "scale(1)  translateY(-40px)"
+        document.querySelector('#topstuff p:nth-child(2)').style.transform = "scale(1)  translateY(-20px)"
     },2000)
 
     setTimeout(function() {
         document.querySelector('#flexy').style.opacity = "1";
-        document.querySelector('#flexy').style.transform = "scale(1) translateY(-50px)";
+        document.querySelector('#flexy').style.transform = "scale(1) translateY(-30px)";
     },4000);
 }
